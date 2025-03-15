d9383b3882c2c8b29ce0cdda45a5479fadabad56
Thiago Lenz <thiagolenz@gmail.com>
2025-03-15 11:33:39 -0300

    Implementacao do facade chamando o service convertendo pra dto

--- src/main/java/com/wishlist/thiagolenz/wishlist/produtos/facade/ProdutosFacade.java
+++ src/main/java/com/wishlist/thiagolenz/wishlist/produtos/facade/ProdutosFacade.java
@@ -1,7 +1,10 @@
 package com.wishlist.thiagolenz.wishlist.produtos.facade;
 
 import com.wishlist.thiagolenz.wishlist.produtos.dto.ProdutoDTO;
+import com.wishlist.thiagolenz.wishlist.produtos.model.ProdutoEntity;
+import com.wishlist.thiagolenz.wishlist.produtos.services.ProdutosService;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,23 +12,49 @@ import java.util.Optional;
 
 @Service
 public class ProdutosFacade {
+    @Autowired private ProdutosService service;
 
     public ProdutoDTO create(ProdutoDTO produto) {
-        return null;
+        ProdutoEntity entidade = convertToEntity(produto);
+        entidade = service.create(entidade);
+        return convertToDTO(entidade);
     }
 
     public ProdutoDTO update(ProdutoDTO produto) {
-        return null;
+        ProdutoEntity entidade = convertToEntity(produto);
+        entidade = service.update(entidade);
+        return convertToDTO(entidade);
     }
 
     public List<ProdutoDTO> getAllByClienteId(Long clienteId) {
-        return null;
+        List<ProdutoEntity> result = service.getAllByCliente(clienteId);
+        return result.stream().map(this::convertToDTO).toList();
     }
 
     public Optional<ProdutoDTO> getByClienteAndNomeProduto(Long clienteId, @NotNull String nomeProduto) {
-        return null;
+        Optional<ProdutoEntity> produto = service.findByProdutoAndClientId(clienteId, nomeProduto);
+        return produto.map(this::convertToDTO);
     }
 
     public void deleteByProdutoId(Long produtoId) {
+        service.deleteById(produtoId);
+    }
+
+    private ProdutoEntity convertToEntity(ProdutoDTO dto) {
+        return new ProdutoEntity(
+                dto.getId(),
+                dto.getNomeProduto(),
+                dto.getClienteId(),
+                dto.getPreco()
+        );
+    }
+
+    private ProdutoDTO convertToDTO(ProdutoEntity entidade) {
+        return new ProdutoDTO(
+                entidade.getId(),
+                entidade.getNomeProduto(),
+                entidade.getClienteId(),
+                entidade.getPreco()
+        );
     }
 }
