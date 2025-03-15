2757b23cdc42c615947678ae5fa1522f3641086a
Thiago Lenz <thiagolenz@gmail.com>
2025-03-15 09:23:07 -0300

    Estrutura inicial criado via SPRING Initializer

---
+++ src/main/java/com/wishlist/thiagolenz/wishlist/WishListThiagoLenzApplication.java
@@ -0,0 +1,13 @@
+package com.wishlist.thiagolenz.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListThiagoLenzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListThiagoLenzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishlist/thiagolenz/wishlist/WishListThiagoLenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlist.thiagolenz.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListThiagoLenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
