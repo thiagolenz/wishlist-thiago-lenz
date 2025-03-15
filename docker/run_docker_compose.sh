ff7c5f1f224c65b71e5093cc229883f2a6480a0e
Thiago Lenz <thiagolenz@gmail.com>
2025-03-15 09:34:54 -0300

    adicionando mongo db com docker compose

---
+++ docker/run_docker_compose.sh
@@ -0,0 +1,9 @@
+printf '=================================================\n'
+printf 'Derrubando Docker\n'
+printf '=================================================\n'
+docker-compose down
+
+printf '=================================================\n'
+printf 'Subindo Docker com base existente\n'
+printf '=================================================\n'
+docker-compose up -d
