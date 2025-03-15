a3284f40b66fa949282ecb4367b8a09d775cb13c
Thiago Lenz <thiagolenz@gmail.com>
2025-03-15 14:44:55 -0300

    Test containers

--- src/test/java/com/wishlist/thiagolenz/wishlist/WishListThiagoLenzApplicationTests.java
+++ src/test/java/com/wishlist/thiagolenz/wishlist/WishListThiagoLenzApplicationTests.java
@@ -2,12 +2,27 @@ package com.wishlist.thiagolenz.wishlist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(classes = WishListThiagoLenzApplication.class)
 class WishListThiagoLenzApplicationTests {
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0").withExposedPorts(27017);
+
+    @DynamicPropertySource
+    static void containersProperties(DynamicPropertyRegistry registry) {
+        mongoDBContainer.start();
+        registry.add("spring.data.mongodb.host", mongoDBContainer::getHost);
+        registry.add("spring.data.mongodb.port", mongoDBContainer::getFirstMappedPort);
+    }
+
 
 	@Test
 	void contextLoads() {
 	}
-
 }
